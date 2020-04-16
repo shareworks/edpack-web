cc9b2497da7649ac1b0b0c6ed58f3613b2b8cd34
kyrylo93 <43705245+kyrylo93@users.noreply.github.com>
2020-04-16 11:48:15 +0300

    add student group index

--- components/StudentsTable/index.vue
+++ components/StudentsTable/index.vue
@@ -3,7 +3,7 @@
     <h3>{{ $t('SW_STUDENTS_TABLE') }}</h3>
     <span>{{ $t('SW_STUDENTS_TABLE_DESCRIPTION') }}</span>
     <!-- Table with students -->
-    <el-table v-show="tableData.length" size="small" :data="getTableData" row-key="_id" ref="studentsTable">
+    <el-table v-show="tableValues.length" size="small" :data="getTableData" row-key="_id" ref="studentsTable">
       <!-- Name -->
       <el-table-column :label="$t('SW_STUDENT')" prop="name" min-width="160">
         <template slot-scope="props">
@@ -27,6 +27,11 @@
           <strong>{{ props.row.groupName }}</strong>
         </template>
       </el-table-column>
+      <el-table-column property="groupName" :label="$t('SW_GROUP_INDEX')" min-width="50">
+        <template slot-scope="props">
+          <strong>{{ props.row.groupIndex }}</strong>
+        </template>
+      </el-table-column>
     </el-table>
 
     <el-pagination class="text-center" background layout="prev, pager, next" :total="total" :page-size="pageSize" @current-change="onPageChange"></el-pagination>
@@ -43,15 +48,35 @@ export default {
     return {
       pageSize: 10,
       currentPage: 0,
+      tableValues: this.tableData,
       total: this.tableData.length,
       course: this.$store.state.course
     }
   },
 
+  created () {
+    let index = 1
+    let groupName = ''
+    const upgradedTableValues = [...this.tableValues]
+
+    upgradedTableValues.forEach(el => {
+      if (groupName !== el.groupName) {
+        index = 1
+        groupName = el.groupName
+        el.groupIndex = index
+      } else {
+        index++
+        el.groupIndex = index
+      }
+    })
+
+    this.tableValues = upgradedTableValues
+  },
+
   computed: {
     getTableData () {
       const takeValuesFrom = this.pageSize * this.currentPage
-      return [...this.tableData].splice(takeValuesFrom, this.pageSize)
+      return [...this.tableValues].splice(takeValuesFrom, this.pageSize)
     }
   },
 
