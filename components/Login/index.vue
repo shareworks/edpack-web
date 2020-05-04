cbcee3d05ce1fffa6da4dad5a1ff21cc1303aaff
flipvanhaaren <flip@shareworks.nl>
2020-05-04 21:00:32 +0200

    add email/password form and reset form

--- components/Login/index.vue
+++ components/Login/index.vue
@@ -1,30 +1,58 @@
 <template>
   <div class="login invisible" v-observe-visibility="{callback: visibilityChanged, throttle: 100, once: true}">
-    <p class="title"><strong>{{ $t('SW_LOGIN') }}</strong></p>
+    <div v-if="!passwordMode">
+      <p class="title"><strong>{{ $t('SW_LOGIN_SCHOOL') }}</strong></p>
 
-    <!-- School selection -->
-    <el-select class="block"
-               v-model="selectedSchool"
-               filterable
-               :placeholder="$t('SW_SELECT_YOUR_SCHOOL')"
-               @change="selectSchool"
-               :no-data-text="$t('SW_NO_DATA')"
-               :no-match-text="$t('SW_NO_SCHOOLS_FOUND')"
-               :loading-text="$t('SW_LOADING')">
-      <el-option v-for="(item, index) in schools" :key="index" :value="item">
-        <i class="icon-school"></i>
-        <span>{{ item.name }}</span>
-      </el-option>
-    </el-select>
+      <!-- School selection -->
+      <el-select class="block"
+                 v-model="selectedSchool"
+                 filterable
+                 :placeholder="$t('SW_SELECT_YOUR_SCHOOL')"
+                 @change="selectSchool"
+                 :no-data-text="$t('SW_NO_DATA')"
+                 :no-match-text="$t('SW_NO_SCHOOLS_FOUND')"
+                 :loading-text="$t('SW_LOADING')">
+        <el-option v-for="(item, index) in schools" :key="index" :value="item">
+          <i class="icon-school"></i>
+          <span>{{ item.name }}</span>
+        </el-option>
+      </el-select>
 
-    <!-- or -->
-    <div class="login-or">{{$t('SW_OR')}}</div>
+      <!-- or -->
+      <div class="login-or">{{$t('SW_OR')}}</div>
 
-    <!-- Google log in -->
-    <el-button class="block" @click="selectGoogle">
-      <span class="google-icon"></span>
-      <strong>{{ $t('SW_LOG_IN_WITH_GOOGLE') }}</strong>
-    </el-button>
+      <!-- Sign in by password -->
+      <el-button class="mb-10 block" type="primary" plain @click="passwordMode = true" v-if="signinByPassword">
+        <i class="icon-user"></i>
+        {{ $t('SW_SIGN_IN_BY_PASSWORD') }}
+      </el-button>
+
+      <!-- Google log in -->
+      <el-button class="block no-margin" @click="selectGoogle">
+        <span class="google-icon"></span>
+        <strong>{{ $t('SW_LOG_IN_WITH_GOOGLE') }}</strong>
+      </el-button>
+    </div>
+
+    <!--  Email/password form -->
+    <div v-if="passwordMode">
+      <el-button type="text" size="small" class="close-password" @click="passwordMode = false">&times;</el-button>
+
+      <p class="title"><strong>{{$t('SW_SIGN_IN_BY_ACCOUNT') }}</strong></p>
+
+      <el-input :placeholder="$t('SW_YOUR_EMAIL_SHORT')" prefix-icon="icon-email" class="mb-5" v-model="form.email"></el-input>
+      <el-input type="password" :placeholder="$t('SW_YOUR_PASSWORD')" prefix-icon="icon-lock" class="mb-10"  v-model="form.password"></el-input>
+
+      <el-button class="mb-10 block" :loading="submitting" type="primary" @click="submitPassword">
+        {{ $t('SW_LOGIN') }}
+        <i class="icon-arrow_forward"></i>
+      </el-button>
+
+      <div class="text-center">
+        <el-button type="text" @click="$router.push({name: 'reset'})" size="small">{{ $t('SW_FORGOT_PASSWORD') }}</el-button>
+      </div>
+
+    </div>
 
     <el-alert class="mt-10" type="error" show-icon v-if="errorType" :title="$t('SW_' + errorType.toUpperCase())"></el-alert>
 
@@ -48,13 +76,16 @@ export default {
   data () {
     return {
       apiUrl: config.api_url,
+      signinByPassword: config.signinByPassword,
+      passwordMode: false,
+      submitting: false,
       selectedSchool: '',
       loading: false,
       schools: [],
-      filteredList: [],
       errorType: this.$route.query.error,
       businessUrl: config.business.url,
-      businessName: config.business.shortName
+      businessName: config.business.shortName,
+      form: { email: '', password: '' }
     }
   },
 
@@ -76,6 +107,25 @@ export default {
       let redirect = this.$route.query.redirect || ''
       if (redirect[0] === '/') redirect = redirect.substr(1)
       window.location.assign(`${this.apiUrl}/auth/google?redirectpath=${redirect}`)
+    },
+    submitPassword () {
+      if (config.mock_user) return this.$router.push('/admin')
+      if (!this.form.password || !this.form.email) return this.$message({ message: this.$i18n.t('SW_EMAIL_PASSWORD_INCOMPLETE'), type: 'error' })
+
+      if (this.submitting) return
+      this.submitting = true
+
+      // Post password here to API
+      this.$http.post('/auth/local')
+        .then((res) => {
+          const redirect = this.$route.query.redirect || ''
+          this.$router.push(redirect)
+        })
+        .catch(() => {
+          this.$message({ message: this.$i18n.t('SW_EMAIL_PASSWORD_INCORRECT'), type: 'error' })
+          this.form.password = ''
+        })
+        .finally(() => { this.submitting = false })
     },
     visibilityChanged (isVisible, entry, count) {
       const { target } = entry
