7eaf2d8cb71907ecc52b20e398a626d29028103c
gino1991 <ginopresilli@gmail.com>
2020-05-06 19:12:48 +0200

    reset -> recover-password

--- pages/Reset/index.vue
+++ pages/Reset/index.vue
@@ -52,7 +52,7 @@ export default {
       if (!this.form.email || this.submitting) return
       this.submitting = true
 
-      this.$http.post('/auth/local/reset')
+      this.$http.post('/auth/local/recover-password')
         .then((res) => {
           this.$message({ message: this.$i18n.t('SW_EMAIL_RESET_SUBMITTED'), type: 'success' })
         })
