6f748ebe64fb43d005a857172cd44ad5569587c3
kyrylo93 <43705245+kyrylo93@users.noreply.github.com>
2020-08-20 13:22:11 +0300

    Update build-oath-url.js

--- utils/build-oath-url.js
+++ utils/build-oath-url.js
@@ -1,10 +1,10 @@
-const buildOathUrl = (prefix = '', lms, schoolId, courseId, isCourseFirst, $http, slug = '', redirectUrl, courseTab, postfix = '') => {
+const buildOathUrl = (prefix = '', lms, schoolId, courseId, isCourseFirst, $http, slug = '', redirectUrl, redirectId, courseTab, postfix = '') => {
 
   const params = isCourseFirst ? `?course=${courseId}&organization=${schoolId}` : `?organization=${schoolId}&course=${courseId}`
 
   let authUrl = `${prefix}/auth/${lms}${params}`
   if ($http.defaults.headers.common['Lti-Access-Token']) authUrl = `${authUrl}&ltiAccessToken=${$http.defaults.headers.common['Lti-Access-Token']}`
-  authUrl = `${authUrl}&redirectpath=${slug}${redirectUrl}/${courseId}/${courseTab}${postfix}`
+  authUrl = `${authUrl}&redirectpath=${slug}${redirectUrl}/${redirectId}/${courseTab}${postfix}`
 
   return authUrl
 }
