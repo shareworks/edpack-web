348888aa859202a230772bc60b75d18ebc1579fa
kyrylo93 <43705245+kyrylo93@users.noreply.github.com>
2020-08-26 14:46:46 +0300

    Update index.vue

--- components/ManageStaff/index.vue
+++ components/ManageStaff/index.vue
@@ -28,7 +28,7 @@
       </el-col>
       <!-- Add instructors -->
       <!-- user.role === 'staff' - for growflow -->
-      <el-col v-if="isSystemAdmin || user.role === 'staff'" :span="12" :xs="24">
+      <el-col v-if="isAdmin" :span="12" :xs="24">
         <users-create :isManageStaff="true" :course="course" :closeDialog="closeDialog" :updateMembers="updateMembers"></users-create>
       </el-col>
     </el-row>
@@ -58,7 +58,7 @@ export default {
       loading: false,
       removing: false,
       sending: false,
-      isSystemAdmin: this.$store.state.user.systemAdmin,
+      isAdmin: this.$store.state.user.role === 'staff',
       form: { recipients: '', toSelf: true }
     }
   },
