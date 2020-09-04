8d7a5c9c91f19acfe842d2f9487b11768099b871
gino1991 <ginopresilli@gmail.com>
2020-09-04 13:23:44 +0200

    Return ilearn logo when course is connected to ilearn

--- utils/get-lms-type.js
+++ utils/get-lms-type.js
@@ -1,4 +1,5 @@
 export default function getLmsType (model) {
   if (model.availableLms.includes('canvas')) return 'canvas'
+  if (model.ilearn && model.ilearn.integration) return 'ilearn'
   return false
 }
