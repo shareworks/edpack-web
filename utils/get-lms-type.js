73bb747e537ff405ba24aa593cb3a12b7f578bbf
gino1991 <ginopresilli@gmail.com>
2020-09-23 09:42:24 +0200

    Fix lms course types

--- utils/get-lms-type.js
+++ utils/get-lms-type.js
@@ -1,5 +1,7 @@
 export default function getLmsType (model) {
   if (model.availableLms.includes('canvas')) return 'canvas'
+  if (model.availableLms.includes('brightspace')) return 'brightspace'
+  if (model.availableLms.includes('blackboard')) return 'blackboard'
   if (model.ilearn && model.ilearn.integration) return 'ilearn'
   return false
 }
