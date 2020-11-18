88806cbd75d2098e593e259d7f34090238bc0e54
gino1991 <ginopresilli@gmail.com>
2020-11-18 16:07:30 +0100

    Fix ilearn.advantage pointer

--- components/CreateCourse/index.vue
+++ components/CreateCourse/index.vue
@@ -51,7 +51,7 @@ export default {
         faculty: '',
         status: this.$store.state.inLTI ? 'inactive' : 'active',
         ilearn: {
-          integration: (this.$store.state.school.ilearn && this.$store.state.school.advantageApiId)
+          integration: (this.$store.state.school.ilearn && this.$store.state.school.ilearn.advantageApiId)
         }
       }
     }
