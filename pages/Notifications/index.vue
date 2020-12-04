80deb7b9c12ce879a0581e299dc19706655b9f46
kyrylo93 <43705245+kyrylo93@users.noreply.github.com>
2020-12-04 12:14:32 +0200

    add Notifications page

---
+++ pages/Notifications/index.vue
@@ -0,0 +1,41 @@
+<template>
+  <div>
+    <animated-landing class="animated"></animated-landing>
+    <el-row type="flex" justify="center" align="middle" class="minimum-page">
+      <el-col>
+        <!-- Show BG -->
+        <div class="public-background"></div>
+
+        <!-- Logo -->
+        <router-link to="/" :aria-label="$t('SW_LOGO_IMAGE')">
+          <div class="horizontal-logo position-relative"></div>
+        </router-link>
+
+        <div class="minimum-container">
+          <h3>{{ $t('SW_NOTIFICATION_DISABLED', [notificationType]) }}</h3>
+          <p>{{ $t('SW_NOTIFICATION_DISABLED_TEXT') }}</p>
+        </div>
+
+        <!-- Footer links -->
+        <footer-links :signin="true"/>
+      </el-col>
+    </el-row>
+  </div>
+</template>
+
+<script>
+import FooterLinks from '../../components/FooterLinks'
+import AnimatedLanding from '../../../components/AnimatedLanding'
+
+export default {
+  name: 'Notifications',
+  metaInfo: { title: 'Reset password' },
+  components: { FooterLinks, AnimatedLanding },
+  computed: {
+    notificationType () {
+      const type = this.$route.query.type
+      return this.$i18n.t(`SW_${type.toUpperCase()}`)
+    }
+  }
+}
+</script>
