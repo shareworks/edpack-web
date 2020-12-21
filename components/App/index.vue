7343b42b7306cfa1cf0b5910e7d801103b159187
flipvanhaaren <flip@shareworks.nl>
2020-12-21 11:18:55 +0100

    fixes

--- components/App/index.vue
+++ components/App/index.vue
@@ -79,11 +79,6 @@ export default {
 
     // Handle resize events
     window.addEventListener('resize', this.handleResize)
-
-    // TODO: Fix for sometimes not updating view when history back or forward is called
-    window.addEventListener('popstate', () => {
-      this.resetKey++
-    })
   },
 
   updated () {
