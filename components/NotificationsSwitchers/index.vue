fa310c204d4c5af7562f157fb1e38b3222ada699
kyrylo93 <43705245+kyrylo93@users.noreply.github.com>
2020-12-22 16:14:28 +0200

    update NotificationsSwitchers

--- components/NotificationsSwitchers/index.vue
+++ components/NotificationsSwitchers/index.vue
@@ -1,39 +1,8 @@
 <template>
   <div>
-    <!-- Disable Staff Notification -->
-    <div v-if="showDisableStaffNotification && (form.role === 'staff' || isAdmin)">
-      <el-switch v-model="form.checks.disableStaffNotifcation" active-color="#13ce66" inactive-color="#ff4949"/>
-      <span class="text-muted ml-10">{{ $t('SW_STAFF_NOTIFICATION_TEXT') }}</span>
-    </div>
-
-    <!-- Disable Start Notification -->
-    <div v-if="showDisableStartNotification">
-      <el-switch v-model="form.checks.disableStartNotification" active-color="#13ce66" inactive-color="#ff4949"/>
-      <span class="text-muted ml-10">{{ $t('SW_STARTED_EVALUATION_NOTIFICATION_TEXT') }}</span>
-    </div>
-
-    <!-- Disable Closed Notification -->
-    <div v-if="showDisableClosedNotification">
-      <el-switch v-model="form.checks.disableClosedNotification" active-color="#13ce66" inactive-color="#ff4949"/>
-      <span class="text-muted ml-10">{{ $t('SW_CLOSED_EVALUATION_NOTIFICATION_TEXT') }}</span>
-    </div>
-
-    <!-- Disable Pending Notification -->
-    <div v-if="showDisablePendingNotification">
-      <el-switch v-model="form.checks.disablePendingNotification" active-color="#13ce66" inactive-color="#ff4949"/>
-      <span class="text-muted ml-10">{{ $t('SW_PENDING_NOTIFICATION_DISABLING_TEXT') }}</span>
-    </div>
-
-    <!-- Disable Released Notification -->
-    <div v-if="showDisableReleasedNotification">
-      <el-switch v-model="form.checks.disableReleasedNotification" active-color="#13ce66" inactive-color="#ff4949"/>
-      <span class="text-muted ml-10">{{ $t('SW_RELEASED_NOTIFICATION_DISABLING_TEXT') }}</span>
-    </div>
-
-    <!-- Disable Released-Locked Notification -->
-    <div v-if="showDisableReleasedLockedNotification">
-      <el-switch v-model="form.checks.disableReleasedLockedNotification" active-color="#13ce66" inactive-color="#ff4949"/>
-      <span class="text-muted ml-10">{{ $t('SW_RELEASED_LOCKED_NOTIFICATION_DISABLING_TEXT') }}</span>
+    <div v-for="notification in notificationRoles">
+      <el-switch v-model="form.checks[notification.type]" active-color="#13ce66" inactive-color="#ff4949"/>
+      <span class="text-muted ml-10">{{ $t(`SW_${notification.type.toUpperCase()}`) }}</span>
     </div>
   </div>
 </template>
@@ -46,13 +15,7 @@ export default {
   data () {
     return {
       isAdmin: this.$store.state.isAdmin,
-      showDisableStaffNotification: config.showDisableStaffNotification,
-      showDisableStartNotification: config.showDisableStartNotification,
-      showDisableClosedNotification: config.showDisableClosedNotification,
-      showDisablePendingNotification: config.showDisablePendingNotification,
-      showDisableReleasedNotification: config.showDisableReleasedNotification,
-      showDisableReleasedLockedNotification: config.showDisableReleasedLockedNotification,
-
+      notificationRoles: config.notificationRoles,
     }
   }
 }
