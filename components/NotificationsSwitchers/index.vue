ce244707113a9b344af52790fbf6ad3405a163b1
kyrylo93 <43705245+kyrylo93@users.noreply.github.com>
2020-12-27 12:42:41 +0200

    show role in disable emails notification

--- components/NotificationsSwitchers/index.vue
+++ components/NotificationsSwitchers/index.vue
@@ -2,6 +2,7 @@
   <div>
     <div v-for="notification in notificationRoles">
       <el-switch v-model="form.checks[notification.type]" active-color="#13ce66" inactive-color="#ff4949"/>
+      <span class="text-faded ml-10">({{ $t('SW_SEE_DISABLE_AS', [translateRoles(notification.role)]) }})</span>
       <span class="text-muted ml-10">{{ $t(`SW_${notification.type.toUpperCase()}`) }}</span>
     </div>
   </div>
@@ -16,6 +17,11 @@ export default {
     return {
       isAdmin: this.$store.state.isAdmin,
       notificationRoles: config.notificationRoles
+    }
+  },
+  methods: {
+    translateRoles (rolesList) {
+      return rolesList.map(role => ' ' + this.$i18n.tc(`SW_${role.toUpperCase()}`, 1).toLowerCase())
     }
   }
 }
