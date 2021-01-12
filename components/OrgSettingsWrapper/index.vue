42c5be49655a33aba5ed8688d7c6194fe9f27212
flipvanhaaren <flip@shareworks.nl>
2021-01-12 20:35:51 +0100

    refactor orgSettings(Wrapper)

--- components/OrgSettingsWrapper/index.vue
+++ components/OrgSettingsWrapper/index.vue
@@ -14,16 +14,17 @@
       <page-header :title="$t('SW_SETTINGS')" icon="icon-lock" :area="$t('SW_ADMIN_ONLY')" :intro="$t('SW_SETTINGS_SHORT', { school: form.name.en })"/>
 
       <!-- Tabs -->
-      <slot name="tabs"/>
+      <el-tabs v-model="toTab" @tab-click="tabClick">
+        <el-tab-pane :label="tab.label" :name="tab.value" :key="tab.value" v-for="tab in tabs"/>
+      </el-tabs>
     </page-cover>
 
     <!-- Settings page content -->
-    <div class="page-body">
+    <div class="page-body" v-if="status === 'done'">
       <el-form ref="form" :model="form" label-width="220px" :label-position="['questions'].includes(settingsMode) ? 'top' : 'left'">
 
-      <!-- Tables -->
-      <slot name="tables"/>
-
+        <!-- Content Component -->
+        <component :is="tabs[settingsMode].name" :form="form"/>
         <!-- Submit or cancel -->
         <el-form-item class="mt-20" v-if="canShowControlsButton">
           <el-button type="primary" @click="onSubmit" class="mr-10" :loading="submitting">{{ $t('SW_SAVE_CHANGES') }}</el-button>
@@ -33,31 +34,50 @@
         </el-form-item>
       </el-form>
     </div>
+
+    <!-- Loading -->
+    <spinner v-else-if="status === 'loading'" class="mt-30"></spinner>
+
+    <!-- Error -->
+    <div class="text-muted text-center mt-30" v-if="status === 'error'">{{ $t('SW_ERROR_LOADING') }}</div>
   </div>
 </template>
 
 <script>
 import Vue from 'vue'
 import VueClipboards from 'vue-clipboards'
+import OrgGeneralSettings from '@/edpack-web/components/OrgGeneralSettings'
+import OrgOptions from '@/components/OrgOptions'
+import OrgFacultiesSettings from '@/edpack-web/components/OrgFacultiesSettings'
+import OrgIntegrationSettings from '@/edpack-web/components/OrgIntegrationSettings'
 
 Vue.use(VueClipboards)
 
 export default {
   name: 'OrgSettingsWrapper',
   metaInfo: { title: 'Settings' },
-  props: ['settingsMode', 'setMode', 'orgLoaded', 'setOrgLoaded', 'form', 'setForm'],
+  props: ['tabs'],
+  components: { OrgGeneralSettings, OrgOptions, OrgFacultiesSettings, OrgIntegrationSettings },
 
   data () {
     return {
       school: Vue.util.extend({}, this.$store.state.school),
-      submitting: false
+      form: Vue.util.extend({}, this.$store.state.school),
+      settingsMode: this.$route.params.settingsMode || 'general',
+      toTab: this.$route.params.settingsMode || 'general',
+      submitting: false,
+      status: 'loading'
     }
+  },
+
+  created () {
+    this.getOrg()
   },
 
   watch: {
     $route (to) {
       if (!to.params.settingsMode) to.params.settingsMode = 'general'
-      if (to.params.settingsMode !== this.settingsMode) this.setMode(to.params.settingsMode)
+      if (to.params.settingsMode !== this.settingsMode) this.settingsMode = to.params.settingsMode
     },
     form: {
       handler () {
@@ -96,10 +116,23 @@ export default {
         .then((res) => {
           // Use JSON.parse(JSON.stringify(obj)) to prevent deep bindings, see Vue.utils.extend issue: https://github.com/vuejs/vue/issues/1849
           this.school = JSON.parse(JSON.stringify(res.data.list[0]))
-          this.setForm(JSON.parse(JSON.stringify(res.data.list[0])))
+          this.status = 'done'
         })
-        .catch(() => { this.$message({ type: 'error', message: this.$i18n.t('SW_GENERIC_ERROR') }) })
-        .finally(() => { this.setOrgLoaded(true) })
+        .catch(() => { this.status = 'error' })
+    },
+    tabClick () {
+      if (this.form.isChanged) {
+        this.$confirm(this.$i18n.t('SW_DISCARD_CHANGED_TEXT'), this.$i18n.t('SW_DISCARD_CHANGED'), {
+          confirmButtonText: this.$i18n.t('SW_DISCARD_AND_CONTINUE'),
+          cancelButtonText: this.$i18n.t('SW_CANCEL')
+        }).then(() => {
+          this.form = JSON.parse(JSON.stringify(this.school))
+          this.settingsMode = this.toTab
+        }).catch(() => { this.toTab = this.settingsMode })
+      } else {
+        this.settingsMode = this.toTab
+        this.$router.replace({ name: 'settings', params: { slug: this.school.slug, settingsMode: this.toTab } })
+      }
     },
     onSubmit () {
       if (this.form.domainError) {
@@ -129,7 +162,6 @@ export default {
           this.$store.state.user.organization = school
           this.$store.dispatch('setUser', this.$store.state.user)
           this.school = JSON.parse(JSON.stringify(school))
-          this.setForm(JSON.parse(JSON.stringify(school)))
           this.$message({ message: this.$i18n.t('SW_CHANGES_SAVED'), type: 'success' })
         })
         .catch(() => { this.$message({ type: 'error', message: this.$i18n.t('SW_GENERIC_ERROR') }) })
