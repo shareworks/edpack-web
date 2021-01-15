b12a42e8c7b2a02eca0ef7ea94c42f3e71872499
kyrylo93 <43705245+kyrylo93@users.noreply.github.com>
2021-01-15 12:32:25 +0200

    Update mergeEmptyLanguageFields.js

--- utils/mergeEmptyLanguageFields.js
+++ utils/mergeEmptyLanguageFields.js
@@ -1,7 +1,9 @@
 const checkValue = (element, currentLanguage, otherLang) => {
-  // both values filled - stop this iteration
+  // If both values filled - stop, no need to copy values
   if (element[currentLanguage] && element[otherLang]) { return null }
-  // add exist value to another side
+  // If both values empty - stop, no need to copy values
+  if (!element[currentLanguage] && !element[otherLang]) { return null }
+  // One values is empty - copy it from another language
   else if (element[currentLanguage]) { element[otherLang] = element[currentLanguage] }
   else { element[currentLanguage] = element[otherLang] }
 }
@@ -10,14 +12,14 @@ const mergeEmptyLanguageFields = (form, currentLanguage) => {
   const otherLang = currentLanguage === 'nl' ? 'en' : 'nl'
 
   for (const key in form) {
-    // run over each element in array
+    // run over each element in array and check its properties
     if (Array.isArray(form[key])) {
       form[key].forEach(element => {
         checkValue(element, currentLanguage, otherLang)
       })
     }
 
-    // check element values
+    // If element has .en property - check is another language filled
     if (form[key].hasOwnProperty('en')) {
       checkValue(form[key], currentLanguage, otherLang)
     }
