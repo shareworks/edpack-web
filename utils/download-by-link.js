3d1077346c5a163dfd6371c8b68a28dc1926846b
kyrylo93 <43705245+kyrylo93@users.noreply.github.com>
2021-01-18 11:25:08 +0200

    Update download-by-link.js

--- utils/download-by-link.js
+++ utils/download-by-link.js
@@ -1,6 +1,6 @@
 export default function downloadByLink (blob, name) {
   const anchor = window.document.createElement('a')
-  anchor.href = window.URL.createObjectURL(blob, { type: 'text/plain' })
+  anchor.href = window.URL.createObjectURL(blob)
   anchor.download = name
   document.body.appendChild(anchor)
   anchor.click() // Prevent IE: "Access is denied"
