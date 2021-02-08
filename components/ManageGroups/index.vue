7b939e8be5b7e1138d1df523d21861b1d2a9b627
kyrylo93 <43705245+kyrylo93@users.noreply.github.com>
2021-02-08 15:06:03 +0200

    Update index.vue

--- components/ManageGroups/index.vue
+++ components/ManageGroups/index.vue
@@ -307,6 +307,7 @@ export default {
 
           this.$message({ message: this.$i18n.t('SW_USERS_UPDATED'), type: 'success' })
         }).catch(err => {
+          console.log(err)
           this.status = 'error'
           this.$message({ message: this.$i18n.t('SW_GENERIC_ERROR'), type: 'error' })
         })
