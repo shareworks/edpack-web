926a2c3d90217a69c2ffef7c8f2917679388a142
flipvanhaaren <flip@shareworks.nl>
2021-03-08 15:03:07 +0100

    fix merge fields for buddycheck team questions

--- utils/merge-empty-language-fields.js
+++ utils/merge-empty-language-fields.js
@@ -17,15 +17,10 @@ const checkValue = (element) => {
 
 const mergeEmptyLanguageFields = (form, props = []) => {
   props.forEach(propertyName => {
-    console.log(propertyName)
     // run over each element in array and check its properties
-    if (Array.isArray(form[propertyName])) {
-      if (form[propertyName].title) { // hack for team questions in Buddycheck
-        return form[propertyName].title.forEach(element => {
-          checkValue(element)
-        })
-      }
+    if (Array.isArray(form[propertyName]) && form[propertyName].length) {
       form[propertyName].forEach(element => {
+        if (element.title) return checkValue(element.title)
         checkValue(element)
       })
     } else if (Object.prototype.hasOwnProperty.call(form[propertyName], 'en')) {
