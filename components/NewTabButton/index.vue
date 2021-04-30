5af739e72a37ad57451b228256c5bf5762be5505
kyrylo93 <43705245+kyrylo93@users.noreply.github.com>
2021-04-30 14:11:28 +0300

    create NewTabButton component

---
+++ components/NewTabButton/index.vue
@@ -0,0 +1,27 @@
+<template>
+  <!-- Open in new tab -->
+  <el-button v-if="newTabButtonVisible && school.openNewLtiTab" size="medium" type="primary" class="button-square-xs mr-5" plain @click="openNewTab">
+    <!-- I propose to use icon-tab-plus from mdi -->
+    <i class="icon-line-plus no-margin"/>
+  </el-button>
+</template>
+
+<script>
+export default {
+  name: 'NewTabButton',
+  data () {
+    const newTabButtonVisible = window.top !== window.self
+    return {
+      newTabButtonVisible,
+      school: this.$store.state.school
+    }
+  },
+
+  methods: {
+    openNewTab () {
+      const url = new URL(window.location + '&fromLtiTab=true')
+      window.open(url.toString())
+    }
+  }
+}
+</script>
