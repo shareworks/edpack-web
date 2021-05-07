7a0e7c7833eb52437ece34dc4327b2520bca5c69
kyrylo93 <43705245+kyrylo93@users.noreply.github.com>
2021-05-07 09:25:01 +0300

    add min max date inputs to Statistics

--- components/Statistics/index.vue
+++ components/Statistics/index.vue
@@ -1,6 +1,31 @@
 <template>
   <div>
     <div v-if="status === 'done'">
+      <el-form class="mb-10">
+        <!-- Start & End date -->
+        <el-form-item :label="$t('SW_STARTEND_DATE')">
+          <el-row type="flex" justify="center" align="middle">
+            <el-col :xs="24" :sm="8">
+              <el-date-picker v-model="minDate" class="auto-width" type="date" format="dd-MM-yyyy" :picker-options="endDateOptions" :placeholder="$t('SW_SELECT_DATE')"/>
+            </el-col>
+            <el-col :xs="24" :sm="4" class="hidden-xs">
+              <div class="text-muted text-center">
+                {{ $t('SW_UNTIL_DATE') }}
+              </div>
+            </el-col>
+            <el-col :xs="24" :sm="8">
+              <el-date-picker v-model="maxDate" class="auto-width" type="date" format="dd-MM-yyyy" :placeholder="$t('SW_SELECT_DATE')" :picker-options="endDateOptions"/>
+            </el-col>
+
+            <el-col :xs="24" :sm="4" class="hidden-xs ml-5">
+              <el-button type="primary" size="medium" class="button-square-xs" plain @click="getStatisticsByDate" :disabled="!minDate && !maxDate">
+                <span class="hidden-xs hidden-sm">{{ $tc('SW_GET_STATISTIC', 1) }}</span>
+              </el-button>
+            </el-col>
+          </el-row>
+        </el-form-item>
+      </el-form>
+
       <!-- completionStats -->
       <masonry :cols="{default: 2, 767: 1}" :gutter="{default: '20px', 767: '10px'}">
         <el-card v-for="(stat, index) in statisticCompletionValues" :key="`statisticCompletionValues${index}`" class="stat-counter">
@@ -53,6 +78,9 @@ export default {
 
   data () {
     return {
+      minDate: '',
+      maxDate: '',
+      endDateOptions: { disabledDate (time) { return new Date(time) > new Date() } },
       school: this.$store.state.school,
       facultyFilter: this.faculty ? this.faculty._id : this.$route.query.context || '',
       statisticCompletionValues: [],
@@ -75,6 +103,14 @@ export default {
   },
 
   methods: {
+    getStatisticsByDate () {
+      this.status = 'loading'
+      const params = { minDate: this.minDate, maxDate: this.maxDate }
+
+      this.$http.get(`organizations/${this.school._id}/counts`, { params })
+        .then(res => { this.setupStatisticValues(res.data.list[0]) })
+        .catch(() => { this.status = 'error' })
+    },
     getFacultyStatistic () {
       this.status = 'loading'
 
