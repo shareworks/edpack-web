a5f762094be1231698062fc5bcb6981aaeda8243
kyrylo93 <43705245+kyrylo93@users.noreply.github.com>
2021-06-24 16:15:46 +0300

    add and use uppy in ThumbnailEdit

--- components/ThumbnailEdit/index.vue
+++ components/ThumbnailEdit/index.vue
@@ -3,9 +3,11 @@
     <!-- Thumbnail -->
     <thumbnail :model="form" class="thumb-user thumb-80 pull-left"/>
     <div class="picture-stuff">
+      <dashboard-modal :uppy="uppy" :props="{theme: 'light', trigger: '.upload-thumbnail-button'}" :open="false" />
+
       <!-- Upload photo -->
-      <el-button class="mb-5" plain @click="pickFile">
-        <i class="icon-camera"></i>
+      <el-button class="mb-5" :class="useUppy && 'upload-thumbnail-button'" plain @click="pickFile">
+        <i class="icon-camera"/>
         <span>{{ $t('SW_UPLOAD_PHOTO') }}</span>
       </el-button>
 
@@ -13,7 +15,7 @@
 
       <!-- Delete -->
       <el-button type="text" :disabled="!form.thumbnailUrl" size="medium" @click="form.thumbnailUrl = ''">
-        <i class="icon-delete"></i>
+        <i class="icon-delete"/>
         <span>{{ $t('SW_DELETE') }}</span>
       </el-button>
     </div>
@@ -21,26 +23,55 @@
 </template>
 
 <script>
+import Tus from '@uppy/tus'
+import Uppy from '@uppy/core'
 import config from 'config'
 import * as filestack from 'filestack-js'
+import { DashboardModal } from '@uppy/vue'
 import convertDiacritics from '../../utils/convert-diacritics'
 
 export default {
   name: 'ThumbnailEdit',
   props: ['form'],
+  components: { DashboardModal },
 
   data () {
     return {
+      useUppy: false,
       filepicker: filestack.init(config.filestack.key)
     }
   },
-
+  computed: {
+    uppy () {
+      return new Uppy({
+        allowMultipleUploads: false,
+        debug: true,
+        showProgressDetails: true,
+        restrictions: {
+          maxTotalFileSize: 2000 * (1024 * 1024),
+          maxNumberOfFiles: 1,
+          minNumberOfFiles: 1,
+          allowedFileTypes: ['image/*']
+        }
+      })
+        .use(Tus, { endpoint: 'https://tusd.tusdemo.net/files/' })
+        .on('complete', result => {
+          console.log('successful files:', result.successful)
+          console.log('failed files:', result.failed)
+        })
+    }
+  },
+  beforeDestroy () {
+    this.uppy.close()
+  },
   methods: {
     pickFile () {
+      if (this.useUppy) return
+
       const customSettings = {
         accept: ['image/*'],
         maxFiles: 1,
-        transformations: { crop: { force: true, aspectRatio: 1 / 1 } },
+        transformations: { crop: { force: true, aspectRatio: 1 } },
         onFileSelected: (file) => {
           file.name = convertDiacritics(file.filename.toLowerCase()).replace(/[^a-zA-Z0-9]/g, '_').replace(/_+/g, '_')
           return file
@@ -69,4 +100,6 @@ export default {
 <style lang="scss">
 @import '~scss_vars';
 @import './style.scss';
+@import "~@uppy/core/dist/style.min.css";
+@import "~@uppy/dashboard/dist/style.min.css";
 </style>
